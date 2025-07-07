fe564c7321c77b54fb5529278f70af20a62bd668
Björn <5106696+b-gehrke@users.noreply.github.com>
2025-07-07 15:57:35 +0200

    Version clearing

--- pywhelk/__init__.py
+++ pywhelk/__init__.py
@@ -11,7 +11,7 @@ def create_reasoner(ontology):
     from os import path, listdir
     
     dir = path.join(path.dirname(__file__), "pywhelk")
-    libname = [f for f in listdir(dir) if any(f.endswith(s) for s in [".so", ".dynlib", ".dll"])][0]
+    libname = [f for f in listdir(dir) if any(f.endswith(s) for s in [".so", ".dylib", ".dll"])][0]
     libpath = path.abspath(path.join(dir, libname))
     
 
